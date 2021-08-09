b62b07dca039f6c4bacad8791cbf109233b269eb
Paulo Cesar <paulocitron99@gmail.com>
2021-08-08 23:27:51 -0300

    add home list

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,29 @@
-import { Text } from '@chakra-ui/react'
+import { Table, TableCaption, Tbody, Td, Th, Thead, Tr } from '@chakra-ui/react'
+
+import useGlobal from 'src/hooks/useGlobal'
 
 function Home(props: any) {
+  const { movies } = useGlobal()
+
   return (
     <div className="home">
-      <Text>Hello world!</Text>
+      <Table variant="striped">
+        <TableCaption>Movies playlist to watch soon!</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>ID</Th>
+            <Th>Name</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
+          {movies.map((movie: any) => (
+            <Tr key={movie.id}>
+              <Td isNumeric>{movie.id}</Td>
+              <Td>{movie.name}</Td>
+            </Tr>
+          ))}
+        </Tbody>
+      </Table>
     </div>
   )
 }
